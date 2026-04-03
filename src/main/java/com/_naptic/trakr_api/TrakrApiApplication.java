3b86fbe1b71726ef4767cd847ae37bd300a27588
INGENIO <ingeniokhalif@gmail.com>
2026-04-03 04:18:07 +0100

    Initial commit

---
+++ src/main/java/com/_naptic/trakr_api/TrakrApiApplication.java
@@ -0,0 +1,13 @@
+package com._naptic.trakr_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrakrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrakrApiApplication.class, args);
+    }
+
+}
